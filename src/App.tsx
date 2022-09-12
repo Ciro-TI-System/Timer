0379b35428395f8078ee248365429913da1e12cf
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-09-12 17:57:22 -0300

    Structuring the navigation.
    Installing the 'React Router Dom' library to configure navigation and create multiple pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
-
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
